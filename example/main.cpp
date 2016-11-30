117cef0065d503812ad38e02bf3062b2bb9a0dae
Андрей <okdahorosho@gmail.com>
2016-12-01 00:42:37 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -12,7 +12,9 @@ int main()
     cout << sum( a, b ) << endl;
     cout << sub( a, b ) << endl;
     cout << mult( a, b ) << endl;
-    cout << div( a, b ) << endl; 
+    double *rez = div(a, b);
+	if (rez) cout << *rez << endl;
+	else cout << "Cant divide by zero" << endl; 
     cout << pow( a ) << endl;
     cout << sq( a ) << endl;
     
