139eef2af84175ee18fb10f49790ac6f57a0c6f9
Андрей <okdahorosho@gmail.com>
2016-12-16 00:02:42 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -20,7 +20,6 @@ int main()
      long double *f = sqrt1(a);
         if (f) cout << "Корень: " << *f << endl;
         else cout << "Из отрицательного числа корень выделить нельзя." << endl;
-        cout << pow1(a, b) << endl;
         delete f; 
     return 0;
 }
